61878a2454e3a0d1ac5498cf8af22926a3cc14c2
rajnandinisarmah <rajnandinisarmah2@gmail.com>
2023-04-01 21:13:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import { Navbar, Feed } from './Components/global';
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h2>❤I LOVE YOU❤</h2>
+        <h1>❤ANSHUMAN TALUKDAR❤</h1>
+        <h1>❤</h1>
+
+        <Navbar />
+        <Feed />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+// import './navbar.scss'
+export default function Navbar() {
+    return (
+        <div></div>
+    )
+}
